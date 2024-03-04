594643ffc7caa0859839014c7c233a73ec1c608c
Yanissou <yanissoumc@gmail.com>
2024-03-04 23:52:50 +0100

    switched from var to let

--- age.js
+++ age.js
@@ -1,7 +1,7 @@
-var now = new Date();
-var naissance = new Date("2004-09-15");
-var age = now.getFullYear() - naissance.getFullYear();
-var moisDiff = now.getMonth() - naissance.getMonth();
+let now = new Date();
+let naissance = new Date("2004-09-15");
+let age = now.getFullYear() - naissance.getFullYear();
+let moisDiff = now.getMonth() - naissance.getMonth();
 if (moisDiff < 0 || (moisDiff === 0 && now.getDate() < naissance.getDate())) {
     age--;
 } 
